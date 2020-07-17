1811ee02dc5a6e2904de8616fc03b5553e31ed32
lalitchilka <lalitchilka@gmail.com>
2020-07-17 23:10:06 +0530

    Initial empty project

---
+++ src/test/java/guru/springframework/api/domain/springrestclientexample/SpringRestClientExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.api.domain.springrestclientexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestClientExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
